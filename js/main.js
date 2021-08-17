98fc7abd6f3bb07b968621f3665487b9affee995
Olza <olza999@inbox.ru>
2021-08-17 16:49:21 +0300

    Profit View init

---
+++ js/main.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  document.querySelector('.menu-toggle').addEventListener('click', function () {
+    document.querySelector('.header-nav').classList.toggle('active');
+    document.querySelector('body').classList.toggle('unscroll');
+    resetMobMenu();
+  })
+
+  let swiperTicker = new Swiper('.ticker-swiper', {
+    spaceBetween: 0,
+    centeredSlides: true,
+    speed: 5000,
+    autoplay: {
+      delay: 0,
+    },
+    loop: true,
+    slidesPerView: 'auto',
+    allowTouchMove: false,
+    disableOnInteraction: false,
+  });
+
+  var swiperSources = new Swiper(".sources-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      1025: {
+        slidesPerView: 3,
+        loop: false,
+        centeredSlides: false,
+      },
+    },
+  });
+});
+
