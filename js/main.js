71ea859145f66c4061583d9dbbb60c22b8b52f62
Olza <olza999@inbox.ru>
2021-08-25 10:22:48 +0300

    Add account history and robots pages

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     resetMobMenu();
   })
 
+  /* Home sliders */
   let swiperTicker = new Swiper('.ticker-swiper', {
     spaceBetween: 0,
     centeredSlides: true,
@@ -19,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
     disableOnInteraction: false,
   });
 
-  var swiperSources = new Swiper(".sources-swiper", {
+  let swiperSources = new Swiper(".sources-swiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     centeredSlides: true,
@@ -32,6 +33,8 @@ document.addEventListener('DOMContentLoaded', function () {
     breakpoints: {
       500: {
         slidesPerView: 2,
+        centeredSlides: true,
+        centeredSlidesBounds: true,
       },
       1025: {
         slidesPerView: 3,
@@ -40,5 +43,67 @@ document.addEventListener('DOMContentLoaded', function () {
       },
     },
   });
+  /* Home sliders end */
+  if (document.querySelector('#subModal')) {
+    document.querySelector('#subModal').addEventListener('shown.bs.modal', (e) => {
+        initSwiperSubs();
+    })
+  } else {
+    initSwiperSubs();
+  }
+  function initSwiperSubs () { 
+    let swiperSubs = new Swiper(".subs-swiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      centeredSlides: true,
+      centeredSlidesBounds: true,
+      pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        clickable: true,
+      },
+      breakpoints: {
+        500: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1025: {
+          slidesPerView: 3,
+          centeredSlides: false,
+        },
+      },
+    });
+  }
+
+  
+
+  /* Account sliders */
+    let swiperRobots = new Swiper('.robots-swiper', {
+      enabled: false,
+      breakpoints: {
+        768: {
+          enabled: true,
+          spaceBetween: 16,
+          slidesPerView: 'auto',
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+              return '<span class="pagination-number ' + className + '">' + (index + 1) + "</span>";
+            },
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        },
+      },
+    });
+    
+
+  
+  /* Account sliders end */
 });
 
